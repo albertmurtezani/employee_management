be4d68e72a8aa2a0265d12ac4d6990c4f5e25e89
Albert Murtezani <am27317@seeu.edu.mk>
2020-06-10 13:03:46 +0200

    Perfundimi

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 app.secret_key = "My Very very Secret Key, Impossible to find"
 
@@ -21,12 +20,61 @@ class Data(db.Model):
         self.phone = phone
 
 
-
-
+# This is the index route where we are going to
+# query on all our employee data
 @app.route('/')
 def Index():
-    return render_template("index.html")
+    all_data = Data.query.all()
+
+    return render_template("index.html", employees=all_data)
 
 
-if __name__ == '__main__':
+# this route is for inserting data to mysql database via html forms
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Employee Inserted Successfully")
+
+        return redirect(url_for('Index'))
+
+
+# this is our update route where we are going to update our employee
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('Index'))
+
+
+# This route is for deleting our employee
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+
